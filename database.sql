dcfc0938f136d000baf998b77b038fa7791a0e9c
Patryk Nowak <patryk.nowak.2012@gmail.com>
2023-04-30 03:37:57 +0200

    improved rent page and added more cars

--- database.sql
+++ database.sql
@@ -12,7 +12,7 @@ CREATE TABLE users(
 );
 
 CREATE TABLE cars(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     brand VARCHAR(128),
 	model VARCHAR(128),
 	horsepower INT,
@@ -21,6 +21,7 @@ CREATE TABLE cars(
 	engine VARCHAR(128),
     drivetrain VARCHAR(128),
     transmission VARCHAR(128),
+    fuel VARCHAR(128),
 	year INT,
 	price INT,
     is_available BOOLEAN
@@ -38,7 +39,7 @@ INSERT INTO users VALUES(
 );
 
 INSERT INTO cars VALUES(
-    1,
+    NULL,
     "BMW",
     "M3 E92",
     420,
@@ -47,28 +48,30 @@ INSERT INTO cars VALUES(
     "4L V8",
     "RWD",
     "manual",
+    "petrol",
     2013,
     1200,
     1
 );
 
 INSERT INTO cars VALUES(
-    2,
+    NULL,
     "Nissan",
     "GTR R35",
     557,
     2.7,
     328,
-    "3.8L V6 (TT)",
+    "3.8L V6 (Twin Turbo)",
     "AWD",
-    "sequential",
+    "automatic_sequential",
+    "petrol",
     2022,
     1800,
     1
 );
 
 INSERT INTO cars VALUES(
-    3,
+    NULL,
     "Audi",
     "R8 V10",
     525,
@@ -76,8 +79,121 @@ INSERT INTO cars VALUES(
     316,
     "5.2 V10",
     "AWD",
-    "sequential",
+    "automatic_sequential",
+    "petrol",
     2019,
     1600,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "Mercedes-Benz",
+    "S 560",
+    469,
+    4.7,
+    250,
+    "4.0 V8",
+    "AWD",
+    "automatic_sequential",
+    "petrol",
+    2017,
+    1100,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "Lamborghini",
+    "Aventador SVJ",
+    759,
+    2.8,
+    352,
+    "6.5 V12",
+    "AWD",
+    "sequential",
+    "petrol",
+    2021,
+    2500,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "Porsche",
+    "911 Turbo S",
+    641,
+    2.7,
+    330,
+    "3.7 B6 (Twin Turbo)",
+    "AWD",
+    "automatic_sequential",
+    "petrol",
+    2020,
+    2200,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "Porsche",
+    "911 GT3 RS",
+    525,
+    3.2,
+    296,
+    "4.0 B6",
+    "RWD",
+    "automatic_sequential",
+    "petrol",
+    2019,
+    2000,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "McLaren",
+    "570S Spider",
+    562,
+    3.2,
+    328,
+    "3.8 V8 (Twin Turbo)",
+    "RWD",
+    "automatic_sequential",
+    "petrol",
+    2018,
+    2400,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "Ferrari",
+    "458 Italia",
+    570,
+    3.5,
+    340,
+    "4.5 V8",
+    "RWD",
+    "sequential",
+    "petrol",
+    2015,
+    2300,
+    1
+);
+
+INSERT INTO cars VALUES(
+    NULL,
+    "Nissan",
+    "370Z",
+    331,
+    5.3,
+    253,
+    "3.7 V6",
+    "RWD",
+    "manual",
+    "petrol",
+    2016,
+    1000,
     1
 );
--- rent.php
+++ rent.php
@@ -20,7 +20,8 @@
         </div>
     </header>
     <main>
-        <div id="rent-container">
+        <div id="rent">
+            <div id="rent-go-back"><a href="javascript:history.go(-1)"><p>&#8592; Powrót</p></a></div>
             <?php
                 if(isset($_GET["auto"])){
                     $conn = mysqli_connect("localhost", "root", "", "frog_car_rental");
@@ -29,16 +30,99 @@
                     $brand = $unformatted[0];
                     array_shift($unformatted);
                     $model = implode(" ", $unformatted);
-
                     $query = mysqli_query($conn, "SELECT * FROM `cars` WHERE brand='".$brand."' AND model='".$model."'");
                     $row = mysqli_fetch_array($query);
 
+                    switch($row["drivetrain"]){
+                        case "AWD":
+                            $drivetrain_loc = "Na cztery koła";
+                            break;
+                        case "RWD":
+                            $drivetrain_loc = "Na tył";
+                            break;
+                        case "FWD":
+                            $drivetrain_loc = "Na przód";
+                            break;
+                    }
+
+                    switch($row["transmission"]){
+                        case "manual":
+                            $transmission_loc = "Manualna";
+                            break;
+                        case "automatic":
+                            $transmission_loc = "Automatyczna";
+                            break;
+                        case "sequential":
+                            $transmission_loc = "Sekwencyjna";
+                            break;
+                        case "automatic_sequential":
+                            $transmission_loc = "Automatyczna / Sekwencyjna";
+                            break;
+                    }
+
+                    switch($row["fuel"]){
+                        case "petrol":
+                            $fuel_loc = "Benzyna";
+                            break;
+                        case "diesel":
+                            $fuel_loc = "Diesel";
+                            break;
+                    }
+
                     echo "<h2>".$brand." ".$model."</h2>";
+                    echo "<div id='rent-container'>";
+                        echo "<div id='rent-description'>";
+                            echo '
+                            <table>
+                                <tr>
+                                    <td><p>moc</p></td>
+                                    <td><p><b>'.$row["horsepower"].' KM</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>0-100</p></td>
+                                    <td><p><b>'.$row["acceleration"].' s</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>prędkość</p></td>
+                                    <td><p><b>'.$row["top_speed"].' km/h</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>silnik</p></td>
+                                    <td><p><b>'.$row["engine"].'</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>napęd</p></td>
+                                    <td><p><b>'.$drivetrain_loc.'</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>skrzynia</p></td>
+                                    <td><p><b>'.$transmission_loc.'</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>paliwo</p></td>
+                                    <td><p><b>'.$fuel_loc.'</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>rocznik</p></td>
+                                    <td><p><b>'.$row["year"].'</b></p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>cena</p></td>
+                                    <td><p><b>'.$row["price"].' za dzień</b></p></td>
+                                </tr>
+                            </table>
+                            ';
 
-                    echo "<pre>";
-                    print_r($row);
-                    echo "</pre>";
+                        echo '<button type="button" id="rent-button" name="rent-button">Wynajmij</button>';
+                        echo "</div>";
 
+                        echo '
+                        <div id="rent-image">
+                            <img src="images/cars/'.$row["id"].'.jpg"/>
+                        </div>
+                        ';
+
+                    echo "</div>";
                     mysqli_close($conn);
                 }
             ?>
